c4cf0b65d1065a35c48c1990e60ef66b8409acf4
yarocruz <yarocruz@gmail.com>
2020-05-22 18:28:12 -0400

    testing google books api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from './utils/API';
 
 function App() {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        loadBook();
+    }, [])
+
+    function loadBook() {
+        API.apiTest()
+            .then(response => {
+                console.log(response.data.items[0].volumeInfo.title)
+                setBooks(response.data.items)
+            })
+            .catch(err => console.log(err));
+    }
+    console.log(books)
     return (
       <div>
         <h1>Hello from React!</h1>
+          {books.length && books[0].volumeInfo.title} {/*Checks if the books array has data.*/}
       </div>
     );
 }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,1 +1,10 @@
 // API KEY AIzaSyDDQAq29aepI77u8zTOZLkZIq0BCAHWTl0
+import axios from 'axios';
+
+const API_KEY = 'AIzaSyDDQAq29aepI77u8zTOZLkZIq0BCAHWTl0';
+
+export default {
+    apiTest: function () {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${API_KEY}`);
+    }
+};
