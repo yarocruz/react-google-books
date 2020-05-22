7ca2683cf380a382088adf89e8a5bbad62e0c1fe
yarocruz <yarocruz@gmail.com>
2020-05-22 19:12:32 -0400

    added material-ui package

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import {TextField} from "@material-ui/core";
 import API from './utils/API';
 
 function App() {
@@ -11,16 +12,18 @@ function App() {
     function loadBook() {
         API.apiTest()
             .then(response => {
-                console.log(response.data.items[0].volumeInfo.title)
-                setBooks(response.data.items)
+                console.log(response.data.items[0].volumeInfo.title);
+                setBooks(response.data.items);
             })
             .catch(err => console.log(err));
     }
-    console.log(books)
+    console.log(books);
     return (
-      <div>
-        <h1>Hello from React!</h1>
-          {books.length && books[0].volumeInfo.title} {/*Checks if the books array has data.*/}
+      <div style={{maxWidth: '1000px', margin: '0 auto'}}>
+        <h1>React Google Book Search</h1>
+          <p>Search for a book.</p>
+          {books.length && books[0].volumeInfo.title} {/*Checks if the books array has data.*/} <br />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       </div>
     );
 }
