6577eb9975f08a86222ebdd4a6d14b0e24d0a6e2
yarocruz <yarocruz@gmail.com>
2020-05-31 20:16:01 -0400

    added Saved and Search page components

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Saved() {
+    return (
+        <h1>The Saved Books should be in this component.</h1>
+    )
+}
---
+++ client/src/pages/Search.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Search() {
+    return (
+        <h1>This will be the search page. Essentially the home page.</h1>
+    )
+}
