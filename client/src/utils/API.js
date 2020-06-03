6c4c1d2adf33a396e7be8d32b5b971b6e9c6bfff
yarocruz <yarocruz@gmail.com>
2020-06-03 14:25:41 -0400

    changed function to receive a query

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const API_KEY = 'AIzaSyDDQAq29aepI77u8zTOZLkZIq0BCAHWTl0';
 
 export default {
-    apiTest: function () {
-        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${API_KEY}`);
+    findBook: function (query) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`);
     }
 };
