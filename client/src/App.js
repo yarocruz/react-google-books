4ba264889a3ce5cacb9d94c8724a0195c36ecdda
yarocruz <yarocruz@gmail.com>
2020-06-03 14:26:35 -0400

    removed the test API call from app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,8 @@
-import React, { useState, useEffect } from "react";
-import API from './utils/API';
+import React from "react";
 import Album from '../src/components/Album';
 import { BrowserRouter as Router} from "react-router-dom";
 
 function App() {
-    const [books, setBooks] = useState([]);
-
-    useEffect(() => {
-        loadBook();
-    }, [])
-
-    function loadBook() {
-        API.apiTest()
-            .then(response => {
-                console.log(response.data.items[0].volumeInfo.title);
-                setBooks(response.data.items);
-            })
-            .catch(err => console.log(err));
-    }
-    console.log(books);
     return (
       <div>
           <Router>
