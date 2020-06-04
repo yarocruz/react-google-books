1c96b78a1b4c9c7fc98e38c52e4b3d6e733ccdb1
yarocruz <yarocruz@gmail.com>
2020-06-04 11:48:46 -0400

    changed the post route back to .create

--- routes/api/book.js
+++ routes/api/book.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bookController = require('../../controllers/bookController');
 
-router.route('/').get(bookController.findAll)
+router.route('/').get(bookController.findAll);
+router.route('/:id').post(bookController.create);
 
 module.exports = router;
