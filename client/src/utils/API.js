850e6acca1365c928b6131585f8782b02b55bef0
yarocruz <yarocruz@gmail.com>
2020-06-04 11:52:22 -0400

    added the book data to the request object

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,5 +8,15 @@ const API_KEY = 'AIzaSyDDQAq29aepI77u8zTOZLkZIq0BCAHWTl0';
 export default {
     findBook: function (query) {
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`);
+    },
+    saveBook: function (bookInfo) {
+        return axios.post(`/${bookInfo.id}`,
+            {
+                title: bookInfo.volumeInfo.title,
+                authors: bookInfo.volumeInfo.authors,
+                description: bookInfo.volumeInfo.description,
+                image: bookInfo.volumeInfo.image,
+                link: bookInfo.volumeInfo.link
+            });
     }
 };
