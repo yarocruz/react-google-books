cee4ceb93dab6bc57ba9ae8769cef1c0f337bc6b
yarocruz <yarocruz@gmail.com>
2020-06-04 11:47:33 -0400

    removed the required field from schemas.

--- schemas/book.js
+++ schemas/book.js
@@ -4,23 +4,18 @@ const Schema = mongoose.Schema;
 bookSchema = new Schema({
     title: {
         type: String,
-        required: true
     },
     authors: {
         type: Array,
-        required: true,
     },
     description: {
         type: String,
-        required: true
     },
     image: {
         type: String,
-        required: true
     },
     link: {
         type: String,
-        required: true
     }
 })
 
