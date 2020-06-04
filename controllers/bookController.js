f38bfe15bf1e7c7e94e7e9527d01a3b1f9500a7e
yarocruz <yarocruz@gmail.com>
2020-06-04 11:51:00 -0400

    added to the create method the whole req object with fields

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,6 +6,18 @@ module.exports = {
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
+    create: function (req, res) {
+        Book
+            .create({
+                title: req.body.title,
+                authors: req.body.authors,
+                description: req.body.description,
+                image: req.body.image,
+                link: req.body.link
+            })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
     remove: function(req, res) {
         Book
             .findById({ _id: req.params.id })
