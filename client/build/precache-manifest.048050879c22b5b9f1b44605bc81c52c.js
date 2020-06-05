0d653fbf8369cfc7648e68b897e91539b9f820de
yarocruz <yarocruz@gmail.com>
2020-06-05 14:06:10 -0400

    setting up to deploy

---
+++ client/build/precache-manifest.048050879c22b5b9f1b44605bc81c52c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4debdfeacf8360191e4d",
+    "url": "/static/js/main.5f780c7e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d4a733e385051d75aa0",
+    "url": "/static/js/2.11e2a84e.chunk.js"
+  },
+  {
+    "revision": "68a2d61b1f7ec1f673c36b6ced145fa7",
+    "url": "/index.html"
+  }
+];
